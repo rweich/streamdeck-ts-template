28a3d09d54a612ea625659f3a462e28a66274868
Roman Weich <roman.weich@gmail.com>
2021-05-24 19:43:18 +0200

    ci: change commitlint config to the conventionalcommits package
    - replace eslint/prettier configs with my config packages
    - remove node 10 from build workflow

--- release.config.js
+++ release.config.js
@@ -3,9 +3,45 @@ const manifestNs = require('./build/scripts/manifest').manifestNs;
 module.exports = {
   branches: 'main',
   plugins: [
-    '@semantic-release/commit-analyzer',
-    '@semantic-release/release-notes-generator',
-    '@semantic-release/changelog',
+    [
+      '@semantic-release/commit-analyzer',
+      {
+        releaseRules: [
+          { type: 'chore', release: 'patch' },
+          { type: 'docs', release: 'patch' },
+          { type: 'refactor', release: 'patch' },
+          { type: 'style', release: 'patch' },
+        ],
+      },
+    ],
+    [
+      '@semantic-release/release-notes-generator',
+      {
+        preset: 'conventionalcommits',
+        presetConfig: {
+          types: [
+            { type: 'feat', section: 'Features' },
+            { type: 'fix', section: 'Bug Fixes' },
+            { type: 'perf', section: 'Performance Improvements' },
+            { type: 'revert', section: 'Reverts' },
+            { type: 'docs', section: 'Documentation' },
+            { type: 'style', section: 'Styles' },
+            { type: 'chore', section: 'Miscellaneous Chores' },
+            { type: 'refactor', section: 'Code Refactoring' },
+            { type: 'test', section: 'Tests', hidden: true },
+            { type: 'build', section: 'Build System', hidden: true },
+            { type: 'ci', section: 'Continuous Integration', hidden: true },
+          ],
+        },
+      },
+    ],
+    [
+      '@semantic-release/changelog',
+      {
+        changelogTitle:
+          '# Changelog\n\nAll notable changes to this project will be documented in this file. See\n[Conventional Commits](https://conventionalcommits.org) for commit guidelines.',
+      },
+    ],
     [
       '@semantic-release/exec',
       {
