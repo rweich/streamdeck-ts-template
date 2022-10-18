10c3a2f44834b2701a6ac07467a42e06709e3f75
Roman Weich <roman.weich@gmail.com>
2022-10-18 21:12:14 +0200

    fix: error in watch-mode
    ... that added the "dev" texts to the manifest every time something
    changed again

--- build/scripts/manifest.js
+++ build/scripts/manifest.js
@@ -4,11 +4,17 @@ module.exports = {
   createDevelopmentManifest: () => {
     const man = manifest;
     man.Actions.map((action) => {
-      action.Name += ' (dev)';
-      action.UUID = 'dev.' + action.UUID;
+      if (!action.Name.endsWith(' (dev)')) {
+        action.Name += ' (dev)';
+      }
+      if (!action.UUID.startsWith('dev.')) {
+        action.UUID = 'dev.' + action.UUID;
+      }
       return action;
     });
-    man.Name += ' (dev)';
+    if (!man.Name.endsWith(' (dev)')) {
+      man.Name += ' (dev)';
+    }
     return JSON.stringify(man, undefined, '\t');
   },
   manifestNs: manifest.Actions[0].UUID.split('.').slice(0, -1).join('.'),
